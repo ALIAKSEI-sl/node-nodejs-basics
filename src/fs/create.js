dd5d89fced9c188a0abfa5c1e6c954f673fdb7b2
Aliaksei <sladkiy.aleksey@gmail.com>
2022-11-30 12:49:53 +0100

    feat: implement fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import { writeFile } from 'node:fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+    const src = `${__dirname}/files/fresh.txt`;
+    const content = 'I am fresh and young';
+    const errorMessage = 'FS operation failed';
+
+    try {
+        await writeFile(src, content, { flag: 'wx' });
+    } catch (e) {
+        throw new Error(errorMessage);
+    }
 };
 
-await create();
+create();
