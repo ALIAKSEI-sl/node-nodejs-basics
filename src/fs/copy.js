6a8897e4f6dabca0ba56003e385258566d0fe768
Aliaksei <sladkiy.aleksey@gmail.com>
2022-11-30 13:58:32 +0100

    feat: implement fs/copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,21 @@
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import { mkdir, readdir, copyFile } from 'node:fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+    const srcFiles = `${__dirname}/files`;
+    const srcFilesCopy = `${__dirname}/files_copy`;
+    const dirFiles = await readdir(srcFiles);
+    const errorMessage = 'FS operation failed';
+    try {
+        await mkdir(srcFilesCopy);
+        await Promise.all(dirFiles.map((elem) => copyFile(`${srcFiles}/${elem}`, `${srcFilesCopy}/${elem}`)))
+    } catch {
+        throw new Error(errorMessage)
+    }
 };
 
 copy();
