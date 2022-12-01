ce90b7d7a358970b251c1c3701bedc248037255c
Aliaksei <sladkiy.aleksey@gmail.com>
2022-12-01 11:51:12 +0100

    feat: implement streams/read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import { createReadStream } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const srcFiles = `${__dirname}/files/fileToRead.txt`;
+    const readStream = createReadStream(srcFiles, { encoding: 'utf8' });
+    readStream.on( 'data', (chunk) => {        
+        console.log(chunk);
+    })
 };
 
-await read();
+read();
