ca9fbccb77469ebae9157c3f3e802af936d46fa6
Aliaksei <sladkiy.aleksey@gmail.com>
2022-12-01 14:29:34 +0100

    feat: reverses text using Transform Stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,28 @@
+import { pipeline, Transform } from 'node:stream';
+import { EOL } from 'node:os';
+
 const transform = async () => {
-    // Write your code here 
+    const reverse = new Transform({
+        transform(chunk, encoding, callback) {
+            const textContent = String(chunk).replace(EOL, '');
+            const arrayReverse = textContent.split('').reverse();
+            const textReverse = arrayReverse.join('') + EOL;
+            callback(null, textReverse);
+        }
+    });
+    pipeline(
+        process.stdin,
+        reverse,
+        process.stdout,
+        (err) => {
+            if (err) {
+                console.error('Unsuccessful', err);
+            } else {
+                console.log('Successfully');
+            }
+        }
+    )
+    //process.stdin.pipe(reverse).pipe(process.stdout)
 };
 
-await transform();
+transform();
