e3c559e727e931da21a4e29aa447f220d7299365
Aliaksei <sladkiy.aleksey@gmail.com>
2022-12-01 15:13:47 +0100

    feat:  implement zip/ compresses file

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import { createWriteStream, createReadStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+    const srcFile = `${__dirname}/files/fileToCompress.txt`;
+    const srcFileZip = `${__dirname}/files/archive.gz`;
+    const readableStream = createReadStream(srcFile, { encoding: 'utf8' });
+    const writeableStream = createWriteStream(srcFileZip);
+    const gzip = createGzip();
+
+    readableStream.pipe(gzip).pipe(writeableStream);
 };
 
-await compress();
+compress();
