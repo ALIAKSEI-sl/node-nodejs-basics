6943b8901f2865ad64a720248c1fe39fb467b46b
Aliaksei <sladkiy.aleksey@gmail.com>
2022-12-01 11:09:05 +0100

    feat: hash/ calculates SHA256 hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createHash } from 'node:crypto';
+import { readFile } from 'node:fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    try{
+    const srcFiles = `${__dirname}/files/fileToCalculateHashFor.txt`;
+    const content = await readFile(srcFiles);
+    const hash = createHash('sha256').update(content).digest('hex');
+    console.log(hash);
+    } catch(error) {
+        throw error;
+    }
 };
 
-await calculateHash();
+calculateHash();
