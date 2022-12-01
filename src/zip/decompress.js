110ac071e8afd806f5614e2ce0a9ad038a2397c7
Aliaksei <sladkiy.aleksey@gmail.com>
2022-12-01 15:32:15 +0100

    feat: implement zip/  decompresses file

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import { createWriteStream, createReadStream } from 'node:fs';
+import { createUnzip } from 'node:zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+    const srcFile = `${__dirname}/files/fileToCompress.txt`;
+    const srcFileZip = `${__dirname}/files/archive.gz`;
+    const readableStream = createReadStream(srcFileZip);
+    const writeableStream = createWriteStream(srcFile);
+    const unzip = createUnzip();
+
+    readableStream.pipe(unzip).pipe(writeableStream);
 };
 
-await decompress();
+decompress();
